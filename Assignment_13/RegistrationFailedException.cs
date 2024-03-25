b7ea647fa34d43a2534542e1207ad179b4130692
robert.badea <48626784+ibor99@users.noreply.github.com>
2024-03-25 09:54:07 +0200

    refactor the speaker class in accordance to clean coding and SOLID principles, add necessary parts for successful compliation

---
+++ Assignment_13/RegistrationFailedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_13
+{
+    public class RegistrationFailedException : Exception
+    {
+        public RegistrationFailedException(string message) : base(message) { }
+        public RegistrationFailedException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ Assignment_13/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_13
+{
+    public static class StringExtensions
+    {
+        public static bool ContainsAny(this string text , IEnumerable<string> keywords)
+        {
+            return keywords.Any(keyword => text.Contains(keyword));
+        }
+    }
+}
